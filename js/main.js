dcdf485fc22b034158522cae533b7d1b367fa8a6
Evgeniy Lebedev <evilhamster@yandex.ru>
2021-06-23 23:22:57 +0300

    23/06

---
+++ js/main.js
@@ -0,0 +1,129 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+class ProductsList {
+    constructor(container = '.products'){
+        this.container = container;
+        this.goods = [];
+        this._getProducts()
+            .then(data => {
+                 this.goods = [...data];
+                 this.render()
+            });
+    }
+
+    _getProducts(){
+        return fetch(`${API}/catalogData.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+    calcSum(){
+        return this.allProducts.reduce((accum, item) => accum += item.price, 0);
+    }
+
+    render(){
+        const block = document.querySelector(this.container);
+        for (let product of this.goods){
+            const productObj = new ProductItem(product);
+            // this.allProducts.push(productObj);
+            block.insertAdjacentHTML('beforeend', productObj.render());
+        }
+    }
+}
+
+
+class ProductItem {
+    constructor(product, img = 'https://via.placeholder.com/200x150'){
+        this.title = product.product_name;
+        this.price = product.price;
+        this.id = product.id_product;
+        this.img = img;
+    }
+    render(){
+        return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="Some img">
+                <div class="desc">
+                    <h3>${this.title}</h3>
+                    <p>${this.price} $</p>
+                    <button class="buy-btn">Купить</button>
+                </div>
+            </div>`
+    }
+}
+
+class CartList {
+    constructor(container = '.cart') {
+        this.container = container;
+        this.cartGoods = [];
+        this._getCartProducts()
+            .then(data => {
+                this.cartGoods = [...data.contents];
+                this.render();
+            })
+    }
+
+    _getCartProducts() {
+        return fetch(`${API}/getBasket.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.cartGoods){
+            const productObj = new CartItem(product);
+            block.insertAdjacentHTML('beforeend', productObj.render());
+        }
+    }
+}
+
+class CartItem {
+    constructor(product, img = 'https://via.placeholder.com/200x150'){
+        this.title = product.product_name;
+        this.price = product.price;
+        this.id = product.id_product;
+        this.quantity = product.quantity;
+        this.img = img;
+    }
+    render(){
+        return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="Some img">
+                <div class="desc">
+                    <h3>${this.title}</h3>
+                    <p>${this.price} $</p>
+                    <p>Количество: ${this.quantity}</p>
+                    <button class="buy-btn">Удалить</button>
+                </div>
+            </div>`
+    }
+}
+
+// Modal
+
+var modal = document.getElementById("my_modal");
+var btn = document.getElementById("btn_modal_window");
+var span = document.getElementsByClassName("close_modal_window")[0];
+
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+let list = new ProductsList();
+let cart = new CartList();
+console.log(list);
+console.log(cart);
+
