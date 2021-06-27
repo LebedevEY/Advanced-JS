bf42aa545fc63824aa65422f17a501e8569d1ed7
Evgeniy Lebedev <evilhamster@yandex.ru>
2021-06-27 21:58:54 +0300

    27/06

--- js/main.js
+++ js/main.js
@@ -103,9 +103,10 @@ class CartItem {
 }
 
 var text = (document.getElementById('first-text'));
-var newText = text.outerHTML.replace(/'/g, '"');
+var newText = text.outerHTML.replace(/'/g, '\"');
+var finalText = newText.replace(/\b"/g, '\'');
 var text2 = document.getElementById('second-text');
-text2.insertAdjacentHTML("beforeend", newText);
+text2.insertAdjacentHTML("beforeend", finalText);
 
 
 // Modal
