186f74e64466f636b22ca801642894f7c4280953
Evgeniy Lebedev <evilhamster@yandex.ru>
2021-06-28 21:22:53 +0300

    28/06

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@
 // Четвёртое домашнее задание
 
 var text = (document.getElementById('first-text'));
-var newText = text.outerHTML.replace(/'/g, '\"');
-var finalText = newText.replace(/\b"/g, '\'');
+var newText = text.outerHTML.replace(/\B'/g, '\"');
 var text2 = document.getElementById('second-text');
-text2.insertAdjacentHTML("beforeend", finalText);
+text2.insertAdjacentHTML("beforeend", newText);
